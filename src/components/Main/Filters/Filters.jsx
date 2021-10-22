ef18bfb88759c6cfb8b2a35f97f157c2b03fb1e5
VladislavMaksimov <workmaximov@yandex.ru>
2021-10-22 21:02:32 +0500

    style filters

--- src/components/Main/Filters/Filters.jsx
+++ src/components/Main/Filters/Filters.jsx
@@ -1,3 +1,18 @@
 import React from "react";
+import "./Filters.css";
 
-export const Filters = () => <div />;
+export const Filters = () => {
+  return (
+    <div className="filters">
+      <label htmlFor="cityPicker">City:</label>
+      <select id="cityPicker" className="filter">
+        <option value="">Lorem Ipsum</option>{" "}
+      </select>
+
+      <label htmlFor="monthPicker">Month:</label>
+      <select id="monthPicker" className="filter">
+        <option value="">Ipsum Lorem</option>{" "}
+      </select>
+    </div>
+  );
+};
