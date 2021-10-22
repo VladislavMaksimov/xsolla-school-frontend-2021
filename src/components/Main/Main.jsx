8f70806a44811359606cef13b51c03778853e655
VladislavMaksimov <workmaximov@yandex.ru>
2021-10-22 21:31:52 +0500

    fetch data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CardsField } from "./CardsField/CardsField";
 import { Filters } from "./Filters/Filters";
 
-export const Main = () => (
-  <main>
-    <Filters />
-    <CardsField />
-  </main>
-);
+export const Main = () => {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    fetch(process.env.REACT_APP_DATA_SOURCE)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
+
+  return (
+    <main>
+      <Filters />
+      <CardsField />
+    </main>
+  );
+};
