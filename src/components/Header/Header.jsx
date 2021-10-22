2f077a74b204e3472c61d1a4473ca01168f3bb95
VladislavMaksimov <workmaximov@yandex.ru>
2021-10-22 20:32:52 +0500

    add project structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Header = () => (
+  <header>
+    <h1> Event Listing </h1>
+  </header>
+);
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CardsField } from "./CardsField/CardsField";
+import { Filters } from "./Filters/Filters";
+
+export const Main = () => (
+  <main>
+    <Filters />
+    <CardsField />
+  </main>
+);
